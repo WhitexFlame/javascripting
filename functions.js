0d3302c44e4bc94b62c4d1bdb8db085b2a8d8c1e
WhitexFlame <WhitexFlame@users.noreply.github.com>
2021-10-19 20:54:58 +0000

    NodeSchool exercises

---
+++ functions.js
@@ -0,0 +1,22 @@
+let eat = function (food) {
+  return food + ' tasted really good.'  
+
+}
+
+console.log(eat('bananas'));
+
+// logs: bananas tasted really good.
+
+
+
+// below logs same result
+
+//  function eat (food) {
+//   return food + ' tasted really good.'
+// }
+
+// console.log(eat('bananas'))
+
+// logs: bananas tasted really good.
+
+
